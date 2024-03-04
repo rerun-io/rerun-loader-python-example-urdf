c94ea6a8e7ac7a1497beed39fb1d7d0e58e2767b
Leonard Bruns <leonardb@kth.se>
2024-03-04 09:55:21 +0100

    Add minimal URDF from C++ example

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <rerun.hpp>
+
+int main() {
+    const auto rec = rerun::RecordingStream("urdf_from_cpp");
+    rec.spawn().exit_on_failure();
+
+    rec.set_time_seconds("testtime", 1.0);
+
+    rec.log_file_from_path("./example.urdf", std::nullopt, "my_urdf/urdf");
+
+    return 0;
+}
